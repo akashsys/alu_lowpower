b2d3c1d92d6d72165f484e2572ed9394dd4c9fe2
akash <akash@local>
2026-01-28 23:16:32 +0530

    tests fixed

--- sources/run_sta_cmd.sh
+++ sources/run_sta_cmd.sh
@@ -4,5 +4,3 @@ export DOCKER_HOST=tcp://host.docker.internal:2375
 # 2. Run the STA tool inside the specialized container
 docker exec openlane bash -c "cd /openlane/PHINITY && /openlane/bin/sta -no_init run_sta.tcl"
 
-# 3. Bring the report back if needed
-docker cp openlane:/openlane/PHINITY/timing_report.rpt ./timing_report.rpt
