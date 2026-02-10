b0188ed7fda3ebba755f9be1cfdf72669b7bdfaf
akash <akash@local>
2026-02-10 14:17:02 +0530

    fixed

--- sources/elastic_credit_arbiter.v
+++ sources/elastic_credit_arbiter.v
@@ -9,18 +9,9 @@ module elastic_credit_arbiter (
 );
 
     localparam MAX_CREDIT = 8'hFF;
-    localparam AGE_THRESH = 8'h20;
-
-    reg [15:0] lfsr_reg;
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) lfsr_reg <= 16'hACE1; 
-        else lfsr_reg <= {lfsr_reg[14:0], lfsr_reg[15] ^ lfsr_reg[13] ^ lfsr_reg[12] ^ lfsr_reg[10]};
-    end
- 
+    localparam AGE_THRESH = 8'h20; 
 
     // TODO: Implement internal state, arbitration logic, and credit system 
     // according to Specification.md
-
-  
 
 endmodule
